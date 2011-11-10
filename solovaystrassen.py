34dc9fb77fa8c8f39e55a097f6f21c357f70fe73
Aditya Vaidya <kroq.gar78@gmail.com>
2011-11-09 20:42:06 -0600

    Added versatility to Solovay-Strassen Test.

--- solovaystrassen.py
+++ solovaystrassen.py
@@ -4,14 +4,21 @@ from jacobi import jacobi
 import random
 import sys
 
-def isPrime(n,k):
-	for i in range(k):
-		a = random.randint(2,n-1)
-		x=jacobi(a,n)
-		if x==0 and (a**((n-1)/2))%n != x:
-			return False
-	return True
-	
+def isPrime(n,k=2,bases=[]):
+	if(bases==[]): # if bases not given, use random 'a'
+		for i in range(k):
+			a = random.randint(2,n-1)
+			x=jacobi(a,n)
+			if x==0 and (a**((n-1)/2))%n != x:
+				return False
+		return True
+	else: # if bases given, use bases 'a' from the array
+		for a in bases:
+			x=jacobi(a,n)
+			if x==0 and (a**((n-1)/2))%n != x:
+				return False
+		return True
+		
 if __name__ == "__main__":
 	if len(sys.argv) < 3:
 		if len(sys.argv) == 2:
