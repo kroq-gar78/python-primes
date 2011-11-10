0799dcf4c569fe1fba234820bb8d316f8d11d3e4
Aditya Vaidya <kroq.gar78@gmail.com>
2011-11-09 20:45:54 -0600

    About to test the new Solovay-Strassen Test.

---
+++ single/solovaystrassen.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+from jacobi import jacobi
+import random
+import sys
+
+def isPrime(n,k=2,bases=[]):
+	if(bases==[]): # if bases not given, use random 'a'
+		for i in range(k):
+			a = random.randint(2,n-1)
+			x=jacobi(a,n)
+			if x==0 and (a**((n-1)/2))%n != x:
+				return False
+		return True
+	else: # if bases given, use bases 'a' from the array
+		for a in bases:
+			x=jacobi(a,n)
+			if x==0 and (a**((n-1)/2))%n != x:
+				return False
+		return True
+		
+if __name__ == "__main__":
+	if len(sys.argv) < 3:
+		if len(sys.argv) == 2:
+			print "WARNING: RECCOMENDED TO INPUT PREFERRED ACCURACY."
+			k = 2
+		else:
+			print "ERROR: NEED AT LEAST 1 NUMERICAL ARGUMENT TO TEST PRIMALITY."
+			exit()
+	else:
+		k = int(sys.argv[2]) # accuracy of the test
+	n = int(sys.argv[1]) # number to test for primality
+
+	print isPrime(n,k)
--- single/solovaystrassen_gen.py
+++ single/solovaystrassen_gen.py
@@ -11,7 +11,7 @@ args = range(int(sys.argv[1]),int(sys.argv[2])+1)
 primesFound = 0
 
 for num in args:
-	if (isPrime(num)):
+	if (isPrime(num,bases=[2,3])):
 		primesFound += 1
 
 writeresults("solovaystrassen",args[len(args)-1],int(primesFound))
