5dc6cdb7a3c9a5e084b72a1136225817d7951bde
Aditya Vaidya <kroq.gar78@gmail.com>
2011-11-09 21:11:12 -0600

    Tested Miller-Rabin Test with versatility.

--- single/millerrabin.py
+++ single/millerrabin.py
@@ -3,45 +3,59 @@
 import random
 import sys
 
-def isPrime(n,k=2):
-	
+def isPrime(n,k=2,bases=[]):
 	if( n == 2 ):
 		return True
-	
 	s=0
 	d=n-1
 	while(d&1==0):
 		d >>= 1
 		s += 1
-	
-	bases = [2,3]
-	
-	for a in bases:
-		nextLoop = False
-		#a = i+2 #WTH?????
-		#a = random.randint(2,n-1)
-		x = pow(a,d)%n
-		if x==1 or x==n-1:
-			continue
-		for r in xrange(s):
-			x = pow(x,2)%n
-			if x==1: #1^2 will always be 1, so it will never reach the next 'if' statement
-				#print "Breakpoint 1", a
+	if(bases==[]): # if bases not given, use random 'a'
+		for i in xrange(k):
+			nextLoop = False
+			a = random.randint(2,n-1)
+			x = pow(a,d)%n
+			if x==1 or x==n-1:
+				continue
+			for r in xrange(s):
+				x = pow(x,2)%n
+				if x==1: #1^2 will always be 1, so it will never reach the next 'if' statement
+					#print "Breakpoint 1", a
+					return False
+				if x==n-1:
+					nextLoop = True
+					break
+			if( not nextLoop ):
+				#print "Breakpoint 2", a
 				return False
-			if x==n-1:
-				nextLoop = True
-				break
-		if( not nextLoop ):
-			#print "Breakpoint 2", a
-			return False
-		#else: continue
-	return True
+			#else: continue
+		return True
+	else: # if bases given, use bases 'a' from the array
+		for a in bases:
+			nextLoop = False
+			x = pow(a,d)%n
+			if x==1 or x==n-1:
+				continue
+			for r in xrange(s):
+				x = pow(x,2)%n
+				if x==1: #1^2 will always be 1, so it will never reach the next 'if' statement
+					#print "Breakpoint 1", a
+					return False
+				if x==n-1:
+					nextLoop = True
+					break
+			if( not nextLoop ):
+				#print "Breakpoint 2", a
+				return False
+			#else: continue
+		return True
 
 if __name__ == "__main__":
 	if len(sys.argv) < 3:
 		if len(sys.argv) == 2:
 			print "WARNING: RECCOMENDED TO INPUT PREFERRED ACCURACY."
-			k = 2
+			#k = 2
 		else:
 			print "ERROR: NEED AT LEAST 1 NUMERICAL ARGUMENT TO TEST PRIMALITY."
 			exit()
@@ -49,4 +63,4 @@ if __name__ == "__main__":
 		k = int(sys.argv[2]) # accuracy of the test
 	n = int(sys.argv[1]) # number to test for primality
 
-	print ifprime(n,k)
+	print isPrime(n,k)
