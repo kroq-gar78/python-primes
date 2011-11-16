c0dfd8ee76a6e5f12507cc14f25a4e3e0651e2c4
Aditya Vaidya <kroq.gar78@gmail.com>
2011-11-15 20:36:04 -0600

    Changed trialdiv_pp.py to have consistent format with the other _pp files.

--- cluster/trialdiv_pp.py
+++ cluster/trialdiv_pp.py
@@ -4,14 +4,13 @@ import sys
 import pp
 from trialdiv import isPrime
 
-args = range(int(sys.argv[1]),int(sys.argv[2])+1)
+args = xrange(int(sys.argv[1]),int(sys.argv[2])+1)
 
-job_server = pp.Server() # uses num. of processors in system
-print "Starting pp with", job_server.get_ncpus(), "workers"
+ppservers=(,)
 
-#jobs = []
-#jobs.append( job_server.submit(isPrime,(args[0],args[1],),(),("math",)) )
-#jobs.append( job_server.submit(isPrime,(args[2],args[3],),(),("math",)) )
+job_server = pp.Server(ppservers=ppservers) # uses num. of processors in system
+
+print job_server.get_active_nodes()
 
 jobs = [(input,job_server.submit(isPrime , (input,), (), ("math",))) for input in args ]
 for input, job in jobs:
