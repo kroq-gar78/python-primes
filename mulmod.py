42034e21a50f7be5e788866ed54859aebba538c8
Aditya Vaidya <kroq.gar78@gmail.com>
2011-12-03 01:02:53 -0600

    Added multiplicative modulus algorithm.

---
+++ mulmod.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# calculates (a*b)%c, taking into account that a*b
+def mulmod(a,b,c):
+	x=0
+	y=a%c
+	while(b>0):
+		if(b&1==0): x = (x+y)%c
+		y = (y*2)%c
+		b >>= 1
+	return x%c
+
+if __name__ == "__main__":
+	print mulmod(int(sys.argv[1]),int(sys.argv[2]),int(argv[3]))
