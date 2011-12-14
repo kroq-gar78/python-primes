ca1da41fbb367b89dd22e2fd67c5b221f993835c
Aditya Vaidya <kroq.gar78@gmail.com>
2011-12-13 20:12:27 -0600

    Slightly modified the Lucas-Lehmer Test.

--- lucaslehmer.py
+++ lucaslehmer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-from mersenne import mersenne
+# Lucas-Lehmer Primality Test for Mersenne Numbers
+
+from trialdiv import isPrime
 
 def lucaslehmer(p):
+	if( not isPrime(p) ): return False # p must be prime
 	s = 4
-	m = mersenne(p)
-	#m = 2**p-1
-	for i in xrange(p-2):
+	m = (1<<p)-1
+	for i in xrange(3,p+1):
 		s = ((s*s)-2)%m
 	return s==0
 
