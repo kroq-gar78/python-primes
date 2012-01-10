ed2f8b2c8f053ea10034e85ef02d327248d0be43
Aditya Vaidya <kroq.gar78@gmail.com>
2012-01-09 21:41:46 -0600

    Added logic for extra amount of jobs.

--- cluster/fermat_pp.py
+++ cluster/fermat_pp.py
@@ -20,15 +20,21 @@ args = xrange(int(sys.argv[1]),int(sys.argv[2])+1)
 ppservers = ()
 
 job_server = pp.Server(ppservers=ppservers) # uses num. of processors in system
+jobs = []
 
 print job_server.get_active_nodes()
 
-primesFound = 0
+rangeStart = (int(sys.argv[1]))
+rangeEnd = (int(sys.argv[2]))
 
-jobs = [(num,job_server.submit(isPrime , (num,int(sys.argv[3])), (expmod,), ("random",))) for num in args ]
-for num, job in jobs:
-	#print num , "is" , job()
-	if( job() == True ):
-		primesFound += 1
+primesFound = 0
+numJobs = (rangeStart-rangeEnd+1)
+jobsPerBatch = 1<<10 # amount of jobs per batch
+jobsInExtraBatch = numJobs%jobsPerBatch
+batchesSent = 0
+# first do the extra batch to reduce the complexity of code
+if jobsInExtraBatch != 0:
+	jobs.append( job_server.submit(batch , (rangeStart,rangeStart+jobsInExtraBatch,int(sys.argv[3]),), (expmod,isPrime,), ("random",)) )
+#while batchesSent*jobsPerBatch+jobsInExtraBatch < (int
 
 writeresults("fermat",int(sys.argv[2])-int(sys.argv[1])+1,int(primesFound))
