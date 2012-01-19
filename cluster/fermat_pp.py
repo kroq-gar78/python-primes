dec3557601e6a37dfc0257a6b996fea6f093f3fa
Aditya Vaidya <kroq.gar78@gmail.com>
2012-01-18 20:48:19 -0600

    Added a bugfix for the batch-division for faster processing.

--- cluster/fermat_pp.py
+++ cluster/fermat_pp.py
@@ -29,8 +29,14 @@ rangeEnd = (int(sys.argv[2]))
 primesFound = 0
 numJobs = (rangeEnd-rangeStart+1)
 #print "Number of jobs:" , numJobs
-jobsPerBatch = 1<<14 # amount of jobs per batch
+if numJobs >= 500000: jobsPerBatch = 1<<14 # amount of jobs per batch
+elif numJobs >= 100000: jobsPerBatch = 1<<12
+else:
+	jobsPerBatch = 1<<10
 jobsInExtraBatch = numJobs%jobsPerBatch
+print "Jobs in extra batch: " + str(jobsInExtraBatch)
+print "Start of extra batch: " + str(rangeStart)
+print "End of extra batch: " + str(rangeStart+jobsInExtraBatch)
 numJobs -= jobsInExtraBatch # remove the modulo just for simplicity
 batchesSent = 0 # amount of batches sent EXCLUDING extra batch
 # first do the extra batch to reduce the complexity of code
@@ -38,19 +44,24 @@ if jobsInExtraBatch != 0:
 	jobs.append( job_server.submit(batch , (rangeStart,rangeStart+jobsInExtraBatch,int(sys.argv[3]),), (expmod,isPrime,), ("random",)) )
 	#print "Extra:" , rangeStart , rangeStart+jobsInExtraBatch
 
+if numJobs <= 0:
+	for job in jobs:
+		primesFound+=int(job())
+	print "No more batches needed."
+	writeresults("fermat",int(sys.argv[2])-int(sys.argv[1])+1,int(primesFound))	
+	exit()
+
 batchRangeStart = 0
 if jobsInExtraBatch == 0:
 	batchRangeStart = rangeStart
 else:
 	batchRangeStart = rangeStart+jobsInExtraBatch
 batchRangeEnd = batchRangeStart+jobsPerBatch
-#print "Start of batch 1:" , batchRangeStart
-#print "End of batch 1:" , batchRangeEnd
-jobs.append( job_server.submit(batch , (batchRangeStart,batchRangeEnd,int(sys.argv[3]),), (expmod,isPrime,), ("random",)) )
+if batchRangeEnd > rangeEnd: batchRangeEnd = rangeEnd
+print "Start of batch 1:" , batchRangeStart
+print "End of batch 1:" , batchRangeEnd
+#jobs.append( job_server.submit(batch , (batchRangeStart,batchRangeEnd,int(sys.argv[3]),), (expmod,isPrime,), ("random",)) )
 batchesSent=1
-
-#while (batchesSent*jobsPerBatch) < numJobs :
-	#jobs.append( job_server.submit(batch , (rangeStart*(batchesSent+1)+jobsInExtraBatch,rangeStart+jobsInExtraBatch,int(sys.argv[3]),), (expmod,isPrime,), ("random",)) )
 
 print "Amount of batches:" , numJobs/jobsPerBatch
 
