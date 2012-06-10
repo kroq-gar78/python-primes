4c7609ed847d24ddced18178dd9527616933230b
Aditya Vaidya <kroq.gar78@gmail.com>
2012-06-10 02:25:00 -0500

    Added fermat_num: finds nth Fermat number, starting at 0

---
+++ fermat_num.py
@@ -0,0 +1,13 @@
+#!/usr/python/env python
+
+# Generate the nth Fermat number (index starting at 0)
+# Created by Aditya Vaidya <kroq.gar78@gmail.com>
+
+import math
+
+def fermat_num(n):
+	return (1<<(2*(n-1)))+1
+
+if __name__ == "__main__":
+	import sys
+	print fermat_num(int(sys.argv[1]))
