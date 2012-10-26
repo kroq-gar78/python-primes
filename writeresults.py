6e84cdfe092d8cf3d1151c6fad25b8aadd7fba8d
Aditya Vaidya <kroq.gar78@gmail.com>
2012-10-26 00:59:13 -0500

    writeresults.py: removed unused 'import io' statement

--- writeresults.py
+++ writeresults.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-# Write the experiments results into a file
-
-import io
+# Write the experiments' results into a file
 
 def writeresults(test,maxnum,primes):
 	f = open(test+"_" + str(maxnum) + ".accuracy",'w')
